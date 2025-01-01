cd22b6e8e13e3eb5c2319771a23a35fc91a0b583
degD <den.ege.der@gmail.com>
2025-01-01 11:47:13 +0300

    test cursor movement

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 
+var cursorIndex = 0;
+var cursorDirection = 1;
+
 /**
  * Move the cursor from left bar (0) to right bar (100). 
  * Numbers out of the bounds will be processed as 0 or 100. 
  * @param {number} i An integer [0, 100]
  */
-function moveCursor(i) {
+function setCursor(i) {
     let rect = document.querySelector(".left-bar").getBoundingClientRect();
     let leftEnd = rect.left + (rect.right - rect.left)/2;
     let rightEnd = window.innerWidth - leftEnd;
@@ -17,6 +20,7 @@ function moveCursor(i) {
 
     let newCursorX = i * (rightEnd - leftEnd) / 100 + leftEnd;
     document.querySelector(".cursor").style["margin-left"] = `${newCursorX - cursorWidth/2}px`;
+    cursorIndex = i;
 }
 
 function useColors(colors) {
@@ -53,3 +57,13 @@ function useDarkTheme() {
         "cursor": "#f2630a"
     });
 }
+
+/* Main */
+useDarkTheme();
+setInterval(() => {
+    let c = cursorIndex;
+    c = c + cursorDirection;
+    setCursor(c);
+    if (c > 100) cursorDirection = -1;
+    if (c < 0) cursorDirection = 1;
+}, 10); // To test cursor movement...
