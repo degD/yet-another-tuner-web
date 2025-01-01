12e62d59a4b1510bcd51c66c3a8039b157b6672d
degD <den.ege.der@gmail.com>
2025-01-01 11:21:39 +0300

    add theme change functions

--- main.js
+++ main.js
@@ -18,3 +18,38 @@ function moveCursor(i) {
     let newCursorX = i * (rightEnd - leftEnd) / 100 + leftEnd;
     document.querySelector(".cursor").style["margin-left"] = `${newCursorX - cursorWidth/2}px`;
 }
+
+function useColors(colors) {
+    let chords = document.querySelectorAll(".chord");
+    [...chords].forEach(element => {
+        element.style["color"] = colors["bar"];
+    });
+    let bars = document.querySelectorAll(".bar");
+    [...bars].forEach(element => {
+        element.style["background-color"] = colors["bar"];
+    });
+    document.querySelector(".horizontal-line")
+    .style["background-color"] = colors["bar"];
+
+    document.querySelector("body")
+    .style["background-color"] = colors["screen"];
+
+    document.querySelector(".cursor")
+    .style["background-color"] = colors["cursor"];
+}
+
+function useLightTheme() {
+    useColors({
+        "bar": "#000000",
+        "screen": "#ececec",
+        "cursor": "#f2630a"
+    });
+}
+
+function useDarkTheme() {
+    useColors({
+        "bar": "#d2d2d2",
+        "screen": "#1a1a1a",
+        "cursor": "#f2630a"
+    });
+}
